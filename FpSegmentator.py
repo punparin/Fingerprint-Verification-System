07a571f6b5f78345fcf7688061174f5fc4cda87c
Jethnithi <bagpigham@gmail.com>
2018-11-03 17:56:23 +0700

    segmentator using sobel filter

--- FpSegmentator.py
+++ FpSegmentator.py
@@ -13,9 +13,17 @@ class FpSegmentator:
         print("   Output - a segmented image")                  #stub
         print("   Output - a mask image (region-of-interest)")  #stub
         segmentedImg = fpImg                                    #stub       
-        maskImg = fpImg                                         #stub  
-        segmentedImg = fpImg                                  
-        maskImg = fpImg                         
+        maskImg = fpImg                                         #stub
+        # kernely = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+        # kernelx = np.array([[1,0,-1],[1,0,-1],[1,0,-1]])
+
+        #sobel
+        kernely = np.array([[-3,-10,-3],[0,0,0],[3,10,3]])
+        kernelx = np.array([[-3,0,3],[-10,0,10],[-3,0,3]])
+
+        edges_x = cv2.filter2D(img,cv2.CV_8U,kernelx)
+        edges_y = cv2.filter2D(img,cv2.CV_8U,kernely) 
+
         rows, cols, *ch = maskImg.shape
         total = 0
         sd = 0
@@ -26,29 +34,38 @@ class FpSegmentator:
                 try:
                     for r in range(row,row + self.blockSize):
                         for c in range(col,col + self.blockSize):
-                            total += maskImg[r,c]
+                            total += edges_x[r,c]
                     for r in range(row,row + self.blockSize):
                         for c in range(col,col + self.blockSize):
-                            sd += (maskImg[r,c] - (total//size))**2
-                    sd = math.sqrt(sd//self.blockSize)
-                    if(total//size < 63) and sd < 160:
-                        for r in range(row,row + self.blockSize):
-                            for c in range(col,col + self.blockSize):
-                                segmentedImg[r,c] = 0
+                            sd += (edges_x[r,c] - (total//size))**2
+                    x_sd = math.sqrt(sd//self.blockSize)
+
+                    for r in range(row,row + self.blockSize):
+                        for c in range(col,col + self.blockSize):
+                            total += edges_y[r,c]
+                    for r in range(row,row + self.blockSize):
+                        for c in range(col,col + self.blockSize):
+                            sd += (edges_y[r,c] - (total//size))**2
+                    y_sd = math.sqrt(sd//self.blockSize)
+
+                    if  x_sd < 200 or y_sd < 400:
+                        for r in range(row,row+16):
+                            for c in range(col,col+16):
+                                segmentedImg[r,c] = 255
                                 
                     total = 0
                     sd = 0
                 except IndexError as ie:
                     pass
-        return segmentedImg, maskImg      
+        return segmentedImg
    
 #-----------------------------
 
 if __name__ == "__main__":
-    img = cv2.imread("1_1.bmp", cv2.IMREAD_GRAYSCALE)
-    segmentator = FpSegmentator(16)
-    maskImg = segmentator.segment(img)
-    cv2.imshow("segment", maskImg)
+    img = cv2.imread("FP DB (subset)/1_1.bmp", cv2.IMREAD_GRAYSCALE)
+    segmentator = FpSegmentator(8)
+    segmentedImg = segmentator.segment(img)
+    cv2.imshow("segment", segmentedImg)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
