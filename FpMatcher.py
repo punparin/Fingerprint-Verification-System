ce577b5027dae1c8bcecaa2d61f855800f663b05
Parin Kobboon <punparin@gmail.com>
2018-11-05 20:23:37 +0700

    Implemented FpMatcher

--- FpMatcher.py
+++ FpMatcher.py
@@ -9,27 +9,34 @@ import MnMatcher
 class FpMatcher:
     def __init__(self):
         #constructing FpSegmentator
+        self.segmentator = FpSegmentator.FpSegmentator(8)
         #constructing FpEnhancer
+        self.enhancer = FpEnhancer.FpEnhancer()
         #constructing MnExtractor
+        self.extractor = MnExtractor.MnExtractor()
         #constructing MnMatcher
-        
+        self.matcher = MnMatcher.MnMatcher()
+
     def match(self, fpImg1, fpImg2):
-        print("Stub - Fingerprint Matching")                #stub
-        print("   Input - a fingerprint image (template)")  #stub
-        print("   Input - a fingerprint image (input)")     #stub
-        print("   Output - similarity score")               #stub
-        similarity = 0.75                                   #stub
-        return similarity                                   #stub
-        
+        segmentedFpImg1 = self.segmentator.segment(fpImg1)
+        enhancedFpImg1 = self.enhancer.enhance(segmentedFpImg1, np.ones(segmentedFpImg1.shape))
+        mnSet1 = self.extractor.extract(enhancedFpImg1)
+
+        segmentedFpImg2 = self.segmentator.segment(fpImg2)
+        enhancedFpImg2 = self.enhancer.enhance(segmentedFpImg2, np.ones(segmentedFpImg2.shape))
+        mnSet2 = self.extractor.extract(enhancedFpImg2)
+
+        return self.matcher.match(mnSet1, mnSet2)
+
 #-----------------------------
 
 if __name__ == "__main__":
     #read fingerprint image 1
-    fpImg1 = cv2.imread("../FP DB1 (train subset)/1_1.bmp", cv2.IMREAD_GRAYSCALE)
+    fpImg1 = cv2.imread("FP DB (subset)/1_1.bmp", cv2.IMREAD_GRAYSCALE)
     cv2.imshow("fp1", fpImg1);
 
     #read fingerprint image 2
-    fpImg2 = cv2.imread("../FP DB1 (train subset)/1_2.bmp", cv2.IMREAD_GRAYSCALE)
+    fpImg2 = cv2.imread("FP DB (subset)/1_2.bmp", cv2.IMREAD_GRAYSCALE)
     cv2.imshow("fp2", fpImg2);
 
     #match two fingerprint images
