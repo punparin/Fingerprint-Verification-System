7551c4097ab6eb22b71ba370bb1eb00dc2f0c490
Parin Kobboon <punparin@gmail.com>
2018-11-06 01:37:36 +0700

    Implemented sorting by distance

--- MnExtractor.py
+++ MnExtractor.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import Binarizer
 import Skeletonizer
+import math
+
 #-----------------------------
 #minutiae types
 M_TYPE_UNKNOWN      = 0
@@ -49,6 +51,9 @@ class MnExtractor:
             return True
         return False
 
+    def distance(self, x1, y1, x2, y2):
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
     def extract(self, enhancedImg):
         rows, cols = enhancedImg.shape
         binImg = self.binarizer.binarize(enhancedImg)
@@ -66,10 +71,14 @@ class MnExtractor:
                     elif cn == 3 and not self.isBoundary(enhancedImg, i, j):
                         mnSet.append([i, j, M_TYPE_BIFURCATION])
 
+        mnSet.sort(key=lambda coord: self.distance(rows // 2, cols // 2, coord[0], coord[1]))
+
         # minutiaImg = cv2.cvtColor(skeletonImg, cv2.COLOR_GRAY2BGR)
         # for i in range(len(mnSet)):
         #     mn = mnSet[i]
-        #     if mn[2] == M_TYPE_ENDPOINT:
+        #     if i < 50:
+        #         minutiaImg[mn[0] - 1: mn[0] + 2, mn[1] - 1: mn[1] + 2] = [0, 255, 0]
+        #     elif mn[2] == M_TYPE_ENDPOINT:
         #         minutiaImg[mn[0] - 1: mn[0] + 2, mn[1] - 1: mn[1] + 2] = [0, 0, 255]
         #     elif mn[2] == M_TYPE_BIFURCATION:
         #         minutiaImg[mn[0] - 1: mn[0] + 2, mn[1] - 1: mn[1] + 2] = [255, 0, 0]
@@ -82,21 +91,24 @@ class MnExtractor:
 #     from FpSegmentator import FpSegmentator
 #     from FpEnhancer import FpEnhancer
 
-#     img = cv2.imread("FP DB (subset)/1_4.bmp", cv2.IMREAD_GRAYSCALE)
-#     cv2.imshow("Original image", img)
+#     for i in range(1, 5):
+#         for j in range(1, 5):
+#             img = cv2.imread("FP DB (subset)/" + str(i) + "_" + str(j) + ".bmp", cv2.IMREAD_GRAYSCALE)
+#             # img = cv2.imread("FP DB (subset)/1_3.bmp", cv2.IMREAD_GRAYSCALE)
+#             cv2.imshow("Original image", img)
 
-#     segmentator = FpSegmentator(8)
-#     segmentedImg = segmentator.segment(img)
-#     cv2.imshow("Segmented image", segmentedImg)
+#             segmentator = FpSegmentator(8)
+#             segmentedImg = segmentator.segment(img)
+#             cv2.imshow("Segmented image", segmentedImg)
 
-#     enhancer = FpEnhancer()
-#     enhancedImg = enhancer.enhance(segmentedImg, np.ones(img.shape))
-#     cv2.imshow("Enhanced image", enhancedImg)
+#             enhancer = FpEnhancer()
+#             enhancedImg = enhancer.enhance(segmentedImg, np.ones(img.shape))
+#             cv2.imshow("Enhanced image", enhancedImg)
 
-#     mnSet = MnExtractor().extract(enhancedImg)
-#     print(len(mnSet))
-#     cv2.waitKey()
-#     cv2.destroyAllWindows()
+#             mnSet = MnExtractor().extract(enhancedImg)
+#             print(len(mnSet))
+#             cv2.waitKey()
+#             cv2.destroyAllWindows()
 
     # for i in range(1, 5):
     #     for j in range(1, 5):
